182c2feeb6181785472a2557afc327da4380ed0b
Gerardo Ramirez <gerardogo145@gmail.com>
2021-04-21 13:02:58 +0200

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import Template from './templates/Template.js';
-console.log('hola');
+//Usamos alias para identificar en donde se encuentra nuestro archivo sin necesidad de especificar toda la ruta
+//El @ indica que es un alias
+import Template from '@templates/Template.js';
+//Agregando los estilos css
+import '@styles/main.css';
+import '@styles/vars.styl';
 
 (async function App() {
   const main = null || document.getElementById('main');
